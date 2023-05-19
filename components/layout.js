2bb46df951ac02e4d0a9b8c0d5869d54d541920c
Caesura Media Net <andyc@caesuramedia.net>
2023-05-19 17:38:45 +0100

    Added metadata.

--- components/layout.js
+++ components/layout.js
@@ -17,18 +17,42 @@ export default function Layout({ children, home }) {
   return (
     <div className={[styles.container, memTylesFont.className].join (' ')}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="A memory game turning over two Tyles and win when they match."
-        />
-        <meta
-          property="og:image"
-          content="/img/back.png"
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-		<meta name="viewport" content="initial-scale=1, width=device-width" />
+		<meta charset="utf-8" />
+		<meta name="format-detection"      content="telephone=yes" />
+		<meta name="format-detection"      content="date=yes" />
+		<meta name="format-detection"      content="address=yes" />
+		<meta name="format-detection"      content="email=no" />
+		<meta name="theme-color"           content="#f1fdf4" />
+		<meta name="description"           content="MemTyles : A memory game where you match all pairs of Tyles." />
+		<meta name="keywords"              content="Memory, Game, Tyles" />
+		<meta name="author"                content="Caesura Media Limited" />
+		<meta name="viewport"              content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+		<title>MemTyles : A memory game</title>
+
+		<link rel="apple-touch-icon"       href="/img/memtyles-logo.png" sizes="180x180" />
+		<link rel="icon"                   href="/img/memtyles-logo.png" sizes="32x32" type="image/png" />
+		<link rel="icon"                   href="/img/memtyles-logo.png" sizes="16x16" type="image/png" />
+		<link rel="mask-icon"              href="/img/memtyles-logo.png" color="#563d7c" />
+		<link rel="icon"                   href="/img/memtyles-logo.png" />
+
+		<meta name="twitter:card"         content="summary" />
+		<meta name="twitter:site"         content="@andykjcragg" />
+		<meta name="twitter:creator"      content="@andykjcragg" />
+		<meta name="twitter:title"        content="MemTyles" />
+		<meta name="twitter:description"  content="MemTyles : A memory game where you match all pairs of Tyles." />
+		<meta name="twitter:image"        content="https://memtyles.vercel.app/img/memtyles-logo.png" />
+
+		<meta property="og:url"              content="https://andykjcragg.co.uk" />
+		<meta property="og:title"            content="MemTyles" />
+		<meta property="og:description"      content="MemTyles : A memory game where you match all pairs of Tyles." />
+		<meta property="og:type"             content="website" />
+		<meta property="og:image"            content="https://memtyles.vercel.app/img/memtyles-logo.png" />
+		<meta property="og:image:secure_url" content="https://memtyles.vercel.app/img/memtyles-logo.png" />
+		<meta property="og:image:type"       content="image/png" />
+		<meta property="og:image:width"      content="50" />
+		<meta property="og:image:height"     content="50" />
       </Head>
       <header className={styles.header}>
       </header>
